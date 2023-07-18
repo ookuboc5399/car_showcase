24708da653cc5455f3cab5db06dbb33a94e8d5c7
ookuboc5399 <ankle0614@gmail.com>
2023-07-18 18:29:45 +0900

    0718

---
+++ components/index.ts
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Searchbar from "./Searchbar";
+import CustomFilter from "./CustomFilter";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    Searchbar,
+    CustomFilter,
+    CarCard,
+    ShowMore,
+}
